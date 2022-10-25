a8a1ee80e254b1f841c496022d897a1b3938c31b
Matthias Personnaz <matthias.personnaz@ecl20.ec-lyon.fr>
2022-10-25 19:53:14 +0200

    Del archives non classées

--- archive_codes_louis/args.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main (int argc, char* argv[]) {
-    for (short i=0; argv[i] != 0; i++) {
-        printf("argv[%d](%p): %s\n", i, argv[i], argv[i]);
-    }
-    return 0;    
-}
--- archive_codes_louis/stackoverflow.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int n = 0;
-
-void main() {
-    printf("%d\n", n++);
-    main();
-}
