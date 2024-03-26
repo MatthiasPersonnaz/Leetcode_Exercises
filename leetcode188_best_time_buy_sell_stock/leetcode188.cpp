217938c1e21a0a92fbc383250030d21a4939a14f
MatthiasPersonnaz <matthias.personnaz@hotmail.fr>
2024-03-26 21:46:34 +0100

    188: solved control flow and created debug output

--- leetcode188_best_time_buy_sell_stock/leetcode188.cpp
+++ leetcode188_best_time_buy_sell_stock/leetcode188.cpp
@@ -10,7 +10,6 @@ using namespace std;
 
 
 
-
 class Solution {
 public:
     int maxProfit(int k, vector<int>& prices)
@@ -30,94 +29,123 @@ public:
         {
             fronts[i] = prices[i+1] - prices[i];
         }
-        fronts[l-1] = 0; // arbitrarily neutral (outside of known time series, not used anyway)
+        fronts[l-1] = 0; // prices[l] set to arbitrarily constant (outside of known time series, not used anyway)
 
 
         // BEGIN TRAVERSAL
         int nb_trans_buy  = 0;
         int nb_trans_sell = 0;
         int pos = 0;
-        int last_trans;
+        int last_trans; // contains either +1 (buy) of -1 (sell)
         int new_trans;
-        int last_price;
+        int last_trans_price; // price of the stock at the last transaction
         int diff_price;
         
+        // First step forward till the first rising front appears
         while (fronts[pos] <= 0)
         {
-            pos++; // 
+            pos++;
         }
-        
-        std::cout << "Fronts \n"; 
-        for (int x : fronts) {std::cout << x << " ";} std::cout << "\n";
-        
+        std::cout << "Position of 1st rising front: pos[" << pos << "] = " << prices[pos] << std::endl;
         if (pos == l) {return 0;} // if function is constantly non-increasing, stop here and return 0 max gain
         
+        // First buy transaction
         transactions[pos] = 1; // otherwise, first positive front, buy
         nb_trans_buy++;
         last_trans = 1;
-        last_price = prices[pos];
+        last_trans_price = prices[pos]; // price of the last transaction
+        std::cout << " * bought first at pos[" << pos << "]" << " for that price." << std::endl;
         
+        std::cout << "- started traverse at pos[" << pos << "] = " << prices[pos] << std::endl;
+
+        // Then iterate forward to sell and buy
         while (pos <= l-1)
         {
+            // Step forward as far as the stock varies monotonically (i.e. either increases or decreases)
             while (fronts[pos] * last_trans >= 0 && pos < l-1)
+            // while the variation is opposite to last transaction (ex last_trans = bought +1 and front >= 0 increases)
             {
-                pos++;
+                pos++; // step forward
             }
+            std::cout << "- stopped traverse at pos[" << pos << "] = " << prices[pos] << std::endl;
 
-            new_trans = -last_trans; 
-            diff_price = prices[pos] - last_price;
+            new_trans = -last_trans; // if last bought, then sell || if last sold, then buy
+            diff_price = prices[pos] - last_trans_price;
+            // stock price difference between the two turning points,
+            // that is, over the course of the single subtimeline where the price was monotonically varying
 
             if (diff_price > 0) // NE PAS FAIRE SI UNIQUEMENT DES 0 à la fin
             {
                 assert(new_trans == -1); // assert we now want to sell at the highest price
                 nb_trans_sell++; // record a sell 
-                gains.push_back(diff_price); // record the gain
+                gains.push_back(diff_price); // record the gain for later constraints
                 transactions[pos] = new_trans;
+                std::cout << "  * sold   at pos[" << pos << "]. Gain over period = " << diff_price << ".\n" << std::endl;
             }
             
             
 
-            if (diff_price <= 0 && pos < l-1) // check if the position is not last
-            // if the position is last, don't buy at this point, end of data time series
+            if (diff_price <= 0 && pos < l-1) // check if the position is not the last
+            // because if the position is last, don't buy at this point, end of data time series
             {
-                assert(new_trans == +1); // assert we now want to buy at the lowest price
+                assert(new_trans == 1); // assert we now want to buy at the lowest price
                 nb_trans_buy++; // record a buy
-                losses.push_back(diff_price); // record the loss
+                losses.push_back(diff_price); // record the loss for later constraints
                 transactions[pos] = new_trans;
+                std::cout << "  * bought at pos[" << pos << "]. Loss over pediod = " << diff_price << "." << std::endl;
             }
 
-            last_price = prices[pos];
+            last_trans_price = prices[pos];
             last_trans = new_trans;
-            pos++;
-        }
-        std::cout << "Transactions \n";
-        for (int x : transactions) {std::cout << x << " ";} std::cout << "\n";
 
-        // at this point the # of buys and sells is necessarily equal because
-        // - we started with a Buy and
-        // - we ended with a Sell
+
+            std::cout << "- started traverse at pos[" << pos << "] = " << prices[pos] << std::endl;
+            pos++;
+
+            
+        }
+
+        // at this point the number of buys and sells is necessarily equal because
+        // * we started with a Buy and
+        // * we ended with a Sell
+        // * the transactions alternate
 
         assert(nb_trans_buy == nb_trans_sell);
 
         // then, operate verification of constraints
-        auto total_gains = std::accumulate(gains.begin(), gains.end(), 0);
+        auto total_gains = std::reduce(gains.begin(), gains.end());
+        std::cout << "TOTAL GAINS = " << total_gains << std::endl;
+        
         
         if (nb_trans_buy < k && nb_trans_sell < k)
         {
             return total_gains;
         }
-        else
+        else // if constraints are not met, there are either too much Buys or Sells
         {
-            std::cout << "Losses \n";
-            for (int x : losses) {std::cout << x << " ";} std::cout << "\n";
-            std::sort(losses.begin(), losses.end());
+            // First let's build a list of all fronts: losses and gains
+            // At this point there should be no losses taken into account in the build of our strategy
+            // But to cut down the number of transactions, we will merge some transaction
+            // We still want to maximize our gains so let's register gradually the smallest losses possible 
+            // by deleting transactions, until the criterion is met.
+            // Don't forget that registered gains from positive fronts can also be deleted.
+            // We count them as misses to win.
+            // So let's buiild the list of losses and negative gains (misses to win) before we order the list.  
+            std::vector<int> additional_negative_transactions(gains); // list the transactions
+            for (int& gain : additional_negative_transactions) {gain = -gain;}
+
+            additional_negative_transactions.insert( additional_negative_transactions.end(), losses.begin(), losses.end() );
+            std::sort(additional_negative_transactions.begin(), additional_negative_transactions.end());
+            std::reverse(additional_negative_transactions.begin(),additional_negative_transactions.end());
             // losses is now sorted
             int res = 0;
             while (nb_trans_buy > k)
             {
-                total_gains += losses[res]; // decrease by the smallest loss and so on
+                total_gains += additional_negative_transactions[res]; // decrease by the smallest loss and so on (elements are negative)
+                std::cout << "deleted one transaction pair and gained " << additional_negative_transactions[res] << std::endl;         
                 res++;
-                nb_trans_buy--;
+                nb_trans_buy--; // this is arbitrary, even if we deleted a sell transaction, since they stand equivalent at this point
+                // in the function
 
             }
         }
@@ -131,14 +159,13 @@ public:
 };
 
 
-
 int main()
 {
     Solution sol; 
-    vector<int> prices = {3,2,6,5,0,3};
-    std::cout << "Prices \n";
-    for (int x : prices) {std::cout << x << " ";} std::cout << "\n";
-    int max_profit = sol.maxProfit(1, prices);
+    vector<int> prices = {3,2,6,5,0,3,4,9,9,10,2,18,17,4,15};
+    std::cout << "Prices = [";
+    for (int x : prices) {std::cout << x << " ";} std::cout << "]\n\n";
+    int max_profit = sol.maxProfit(2, prices);
     std::cout << "Max_profit: " << max_profit << "\n";
     return 0;
 }
